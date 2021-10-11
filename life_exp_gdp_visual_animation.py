0b051e10b0831b9ae20d8b2912d3326ab1c9cab9
rmerzouki <reda.merzouki@yahoo.fr>
2021-10-11 15:48:42 +0200

    Update life_exp_gdp_visual_animation.py

--- life_exp_gdp_visual_animation.py
+++ life_exp_gdp_visual_animation.py
@@ -8,7 +8,7 @@ gapminder_df = px.data.gapminder()
 animation_title = '<p style="font-family:Arial Bold; color:royalblue; font-size: 30px;"> Animated Data Visualization</p>'
 sub_title1 = '<p style="font-family:Arial Bold Italic; color:royalblue; font-size: 20px;">This animation shows world life expectancy and wealth (GDP per capita) evolution from 1952 to 2007</p>'
 sub_title2 = '<p style="font-family:Arial Bold Italic; color:royalblue; font-size: 15px;">Source: https://www.gapminder.org/data/</p>'
-sub_title3 = '<p style="font-family:Arial Bold Italic; color:royalblue; font-size: 10px;">Author: Reda Merzouki, https://www.linkedin.com/in/reda-merzouki-02843b/</p>' 
+sub_title3 = '<p style="font-family:Arial Bold Italic; color:royalblue; font-size: 12px;">Author: Reda Merzouki, https://www.linkedin.com/in/reda-merzouki-02843b/</p>' 
 st.set_page_config(layout='wide')
 st.markdown(animation_title, unsafe_allow_html=True)
 st.markdown(sub_title1, unsafe_allow_html=True)
