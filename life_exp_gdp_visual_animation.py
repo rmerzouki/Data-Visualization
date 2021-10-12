46ee01dde3546ef9ce00c388d61b7d2179b1e02f
rmerzouki <reda.merzouki@yahoo.fr>
2021-10-12 16:15:16 +0200

    Update life_exp_gdp_visual_animation.py

--- life_exp_gdp_visual_animation.py
+++ life_exp_gdp_visual_animation.py
@@ -5,7 +5,7 @@ import streamlit as st
 #import the data available in plotly.express
 gapminder_df = px.data.gapminder()
 
-animation_title = '<p style="font-family:Arial Bold; color:royalblue; font-size: 30px;"> Animated Data Visualization</p>'
+animation_title = '<p style="font-family:Arial Bold; color:royalblue; font-size: 30px;"> Animated & Interactive Data Visualization</p>'
 sub_title1 = '<p style="font-family:Arial Bold Italic; color:royalblue; font-size: 20px;">This animation shows world life expectancy and wealth (GDP per capita) evolution from 1952 to 2007</p>'
 sub_title2 = '<p style="font-family:Arial Bold Italic; color:royalblue; font-size: 15px;">Source: https://www.gapminder.org/data/</p>'
 sub_title3 = '<p style="font-family:Arial Bold Italic; color:royalblue; font-size: 12px;">Author: Reda Merzouki, https://www.linkedin.com/in/reda-merzouki-02843b/</p>' 
